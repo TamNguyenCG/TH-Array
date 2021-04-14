c1b5c1e61091b702818944f01274467c2365a185
TamNguyenCG <ngductam91@gmail.com>
2021-04-14 11:30:50 +0700

    Tìm Max trong Array

--- Bai4/JS4.js
+++ Bai4/JS4.js
@@ -1,10 +1,10 @@
 let numbers = [-3, 5, 1, 3, 2, 10];
-let min = numbers[0];
+let max = numbers[0];
 let i = 1;
 for (; i < numbers.length; i++) {
-    if (min > numbers[i]){
-        min = numbers[i]
+    if (max < numbers[i]){
+        max = numbers[i]
     }
 }
 document.write(`x = [${numbers}] <br>`)
-document.write(`Min is: ${min}`)
+document.write(`Max is: ${max}`)
